b07124e51ec708c4a46f0133f2b732da6e986bee
aramogs <33231776+aramogs@users.noreply.github.com>
2020-10-22 10:09:14 -0600

    Se agrega opcion para cerrar sesion

--- public/js/functions/main_menu.js
+++ public/js/functions/main_menu.js
@@ -1,6 +1,7 @@
 let btn_pt_1 = document.getElementById("btn_pt_1")
 let btn_pt_2 = document.getElementById("btn_pt_2")
 let btn_mp_1 = document.getElementById("btn_mp_1")
+let btn_logoff = document.getElementById("btn_logoff")
 
 btn_pt_1.addEventListener("click",()=>{
     window.location.replace(window.location.origin + "/consultaFG")
@@ -14,3 +15,4 @@ btn_mp_1.addEventListener("click", ()=>{
     window.location.replace(window.location.origin + "/movimiento_parcial")
 })
 
+btn_logoff.addEventListener("click",()=>{ document.cookie = "accessToken" + '=; Max-Age=0', location.reload()})
