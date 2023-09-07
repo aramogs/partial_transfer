fb22077e61d81c53d19bb34e48d01a524c3cbb68
TristoneAG <aram.guillen@tristone.com>
2023-09-07 11:28:01 -0600

    fix: :bug: Fixing EXT Audit
    Fixing EXT Audit, the new code was returning a different message

--- routes/routesController.js
+++ routes/routesController.js
@@ -1916,15 +1916,7 @@ controller.auditoriaEXT_POST = async (req, res) => {
         );
 
         const results = await Promise.all(promises);
-        const errors = results
-            .filter(result => result.error) // Extract error objects
-            .map(result => result.error); // Extract just the error object
-
-        if (errors.length > 0) {
-            res.json(errors);
-        } else {
-            res.json({ success: true });
-        }
+        res.json(results)
     } catch (err) {
         res.json(err);
     }
